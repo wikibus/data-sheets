0907d5e2b39523fdd62a23531b6faae040183d89
tpluscode <tpluscode@users.noreply.github.com>
2019-04-17 20:01:19 +0200

    add linting

--- index.js
+++ index.js
@@ -8,49 +8,49 @@ const program = require('commander')
 require('dotenv').config()
 
 function logger (req, res, next) {
-    process.stdout.write(`${req.method} ${req.url} \n`)
+  process.stdout.write(`${req.method} ${req.url} \n`)
 
-    res.on('finish', () => {
-        process.stdout.write(`${res.statusCode}\n`)
-    })
+  res.on('finish', () => {
+    process.stdout.write(`${res.statusCode}\n`)
+  })
 
-    next()
+  next()
 }
 
 function hydraMiddleware () {
-    const sparqlQueryEndpoint = process.env.SPARQL_QUERY_ENDPOINT
-    const sparqlUpdateEndpoint = process.env.SPARQL_UPDATE_ENDPOINT
+  const sparqlQueryEndpoint = process.env.SPARQL_QUERY_ENDPOINT
+  const sparqlUpdateEndpoint = process.env.SPARQL_UPDATE_ENDPOINT
 
-    return hydraBox.fromUrl('/api', 'file://' + path.join(__dirname, 'hydra/apidoc.ttl'), {
-        debug: true,
-        sparqlEndpointQueryUrl: sparqlQueryEndpoint,
-        sparqlEndpointUpdateUrl: sparqlUpdateEndpoint,
-        contextHeader: '/context/'
-    })
+  return hydraBox.fromUrl('/api', 'file://' + path.join(__dirname, 'hydra/apidoc.ttl'), {
+    debug: true,
+    sparqlEndpointQueryUrl: sparqlQueryEndpoint,
+    sparqlEndpointUpdateUrl: sparqlUpdateEndpoint,
+    contextHeader: '/context/'
+  })
 }
 
 program
-    .option('--port <port>', '', 12345)
-    .action(({ port }) => {
-        Promise.resolve().then(async () => {
-            const baseUrl = `http://localhost:${port}/`
+  .option('--port <port>', '', 12345)
+  .action(({ port }) => {
+    Promise.resolve().then(async () => {
+      const baseUrl = `http://localhost:${port}/`
 
-            const app = express()
+      const app = express()
 
-            app.enable('trust proxy')
-            app.use(logger)
-            app.use(cors({
-                exposedHeaders: ['link', 'location']
-            }))
-            app.use(await hydraMiddleware())
+      app.enable('trust proxy')
+      app.use(logger)
+      app.use(cors({
+        exposedHeaders: ['link', 'location']
+      }))
+      app.use(await hydraMiddleware())
 
-            const server = app.listen(url.parse(baseUrl).port, () => {
-                const host = server.address().address
-                const port = server.address().port
+      const server = app.listen(new url.URL(baseUrl).port, () => {
+        const host = server.address().address
+        const port = server.address().port
 
-                console.log(`listening at http://${host}:${port}`)
-            })
-        }).catch(err => console.error(err))
-    })
+        console.log(`listening at http://${host}:${port}`)
+      })
+    }).catch(err => console.error(err))
+  })
 
 program.parse(process.argv)
