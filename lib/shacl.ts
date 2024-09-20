fb02fd1d905c68731d1f729c75295700b8d0b7d9
tpluscode <tpluscode@users.noreply.github.com>
2024-09-20 22:25:53 +0200

    feat: playground link to failures

--- lib/shacl.ts
+++ lib/shacl.ts
@@ -1,14 +1,26 @@
 import { Quad } from '@rdfjs/types';
 import SHACLValidator from 'rdf-validate-shacl'
 import rdf from '@zazuko/env-node'
-import {Context} from "barnard59-core";
+import { Context } from "barnard59-core";
+import { createPlaygroundUrl } from '@zazuko/shacl-playground'
+import { shorten } from '@zazuko/s'
+import pretty from '@rdfjs-elements/formats-pretty'
 
+rdf.formats.import(pretty)
 const shapes = rdf.dataset().import(rdf.fromFile(new URL('../shape/DataSheet.ttl', import.meta.url)));
+
+const prefixes = [
+  'sh', 'mads', 'xsd', 'schema', 'rdf', 'rdfs', 'unit', 'qudt', 'dcterms',
+  ['wb', 'http://www.wikidata.org/entity/'],
+  ['wikibus', 'https://schema.wikibus.org/'],
+]
 
 export async function chunkIsValid(this: Context, chunk: Quad[]): Promise<Iterable<Quad>> {
   const documentId = this.variables.get('documentId');
-  const validator = new SHACLValidator(await shapes);
+  const shapesGraph = await shapes;
+  const validator = new SHACLValidator(shapesGraph);
 
+  const dataGraph = this.env.dataset(chunk);
   const report = validator.validate(this.env.dataset(chunk));
 
   if (report.conforms) {
@@ -16,7 +28,10 @@ export async function chunkIsValid(this: Context, chunk: Quad[]): Promise<Iterab
   } else {
     this.error(new Error('Some data sheets failed validation'));
     const results = report.results.length;
-    this.logger.warn(`Document ${documentId}: ${results} validation issues found`)
+
+    const playgroundLink = await shorten(await createPlaygroundUrl(shapesGraph, dataGraph, { env: rdf, prefixes }));
+    this.logger.warn(`Document ${documentId}: ${results} validation issues found. Check ${playgroundLink}`)
+
     return [...report.dataset, ...chunk].map(renameReportQuad(documentId));
   }
 }
